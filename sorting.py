a959f4799dbc5b530fd77ac4865946e38bd57d7d
284483 <284483@vutbr.cz>
2026-04-21 13:48:46 +0200

    ok

--- sorting.py
+++ sorting.py
@@ -3,6 +3,21 @@ import random
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
+def selection_sort(zoznam):
+    cisla=zoznam.copy()
+    n=len(cisla)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if cisla[j] < cisla[min_idx]:
+                min_idx = j
+
+        # Prohodíme nalezené minimum s prvkem na pozici i
+        cisla[i], cisla[min_idx] = cisla[min_idx], cisla[i]
+
+    return cisla
+
+
 def bubble_sort(zoznam):
     n = len(zoznam)
     for i in range(n):
@@ -14,6 +29,7 @@ def bubble_sort(zoznam):
 def main():
     cisla_test=(random_numbers(20))
     print(cisla_test)
+    print(selection_sort(cisla_test))
     print(bubble_sort(cisla_test))
 
 
